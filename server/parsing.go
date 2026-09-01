9859bc340f57bc418e2aa3e87f35a6baa3cfc515
mishamoskalenko <misamoskalenko318@gmail.com>
2026-09-01 16:13:10 +0200

    added parsing

---
+++ server/parsing.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type GameWorld struct {
+	World World
+}
+
+type World struct {
+	Locations []Location
+	Items     []Item
+	NPCs      []NPC
+}
+
+type Spawn struct {
+	NpcType string
+	Count   int
+}
+
+type Location struct {
+	Id          string
+	Name        string
+	Description string
+	Exits       map[string]string
+	Spawns      []Spawn
+	Items       []string
+}
+
+type Item struct {
+	Id          string
+	Name        string
+	Description string
+	Obtainable  bool
+}
+
+type NPC struct {
+	Id          string
+	Name        string
+	Description string
+	Dialogue    []string
+	Stats       map[string]int
+}
+
+func parsing(fileName string) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+	var game GameWorld
+	err = json.Unmarshal(data, &game)
+	if err != nil {
+		fmt.Println("Error parsing:", err)
+		return
+	}
+	for _, loc := range game.World.Locations {
+		fmt.Printf("[%s] %s: %s\n", loc.Id, loc.Name, loc.Description)
+	}
+	for _, item := range game.World.Items {
+		fmt.Printf("[%s] %s (%t)\n", item.Id, item.Name, item.Obtainable)
+	}
+	for _, npc := range game.World.NPCs {
+		fmt.Printf("[%s] %s (HP: %d)\n", npc.Id, npc.Name, npc.Stats["hp"])
+	}
+}
+
